4ed698fad0165fb0dcc20b0e873671ce5bf1cbaa
Phạm Nhớ <Phamnho9x@gmail.com>
2020-06-26 18:43:37 +0700

    Colours & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Chào Nhớ!'), // Nội dung Title
-            centerTitle: true, // Căn giữa Title
+        appBar: AppBar(
+          title: Text(
+            'Chào Nhớ!',
+            style: TextStyle(fontFamily: 'Pangolin'),
+          ), // Nội dung Title
+          centerTitle: true, // Căn giữa Title
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: Center(
+          // Căn giữa nội dung của body bằng Center
+          child: Text(
+            'Đây là body',
+            style: TextStyle(
+              //cỡ chữ
+              fontSize: 20.0,
+              //in đậm
+              fontWeight: FontWeight.bold,
+              //khoảng cách giữa các chữ
+              letterSpacing: 2.0,
+              //màu chữ
+              color: Colors.grey[600],
+              // Font chữ
+              fontFamily: 'Pangolin',
+            ),
           ),
-          body: Center(// Căn giữa nội dung của body
-            child: Text('Đây là body'),
-          ),
-          floatingActionButton: FloatingActionButton(
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
           child: Text('Click'),
+          backgroundColor: Colors.deepPurpleAccent,
         ),
       ),
     ));
